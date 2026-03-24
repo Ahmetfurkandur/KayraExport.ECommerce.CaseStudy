9d5a1becd65d0edf889f292ed02446e898344fc1
Ahmetfurkandur <fkn3388@hotmail.com>
2026-03-24 21:31:20 +0300

    feat: Added TokenService and ITokenService, added JWT environment variables to docker-compose.yml

---
+++ src/AuthService/AuthService.Application/Interfaces/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using AuthService.Domain.Entities;
+
+namespace AuthService.Application.Interfaces
+{
+    public interface ITokenService
+    {
+        string GenerateAccessToken(AppUser user, List<string> roles);
+        string GenerateRefreshToken();
+    }
+}
---
+++ src/AuthService/AuthService.Application/Services/TokenService.cs
@@ -0,0 +1,48 @@
+﻿using AuthService.Application.Interfaces;
+using AuthService.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AuthService.Application.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string GenerateAccessToken(AppUser user, List<string> roles)
+        {
+            var claims = new List<Claim>()
+            {
+                new(ClaimTypes.NameIdentifier, user.Id),
+                new(ClaimTypes.Email, user.Email!)
+            };
+            claims.AddRange(roles.Select(r => new Claim(ClaimTypes.Role, r)));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT_SECRET_KEY"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["JWT_ISSUER"],
+                audience: configuration["JWT_AUDIENCE"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(configuration["JWT_ACCESS_EXPIRY_MIN"])),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public string GenerateRefreshToken()
+        {
+            return Convert.ToBase64String(RandomNumberGenerator.GetBytes(64));
+        }
+    }
+}
