6ac5a3323c06c4f6ce5e19521784ee53efb4cf03
Ahmetfurkandur <fkn3388@hotmail.com>
2026-03-25 21:54:32 +0300

    feat: add YARP gateway with JWT auth & rate limiting

--- api-gateways/Gateway/Gateway.API/Program.cs
+++ api-gateways/Gateway/Gateway.API/Program.cs
@@ -1,8 +1,43 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
-builder.Services.AddControllers();
+//FixedWindow algortimasıyla rate limiting entegrasyonu yapıyoruz. Daha detaylı bilgi için https://www.linkedin.com/posts/ahmet-furkan-dur-2068b8214_aspnetcore-ratelimiting-dotnet-activity-7207872002118836225-mpMT?utm_source=share&utm_medium=member_desktop&rcm=ACoAADY9EiYB5azuXBOwBALs0h48zB08-r28004
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("fixed", opt =>
+    {
+        opt.Window = TimeSpan.FromMinutes(1);
+        opt.PermitLimit = 60;
+        opt.QueueProcessingOrder = System.Threading.RateLimiting.QueueProcessingOrder.OldestFirst;
+        opt.QueueLimit = 5;
+    });
+});
+
+// Merkezi authentication sistemi
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JWT_ISSUER"],
+            ValidAudience = builder.Configuration["JWT_AUDIENCE"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JWT_SECRET_KEY"]!))
+        };
+    });
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,8 +51,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapControllers();
+app.UseRateLimiter();
+app.MapReverseProxy();
 
 app.Run();
