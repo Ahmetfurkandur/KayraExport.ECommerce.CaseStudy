e3ccb561a42adbabd7dce5a07019771523c72f32
Ahmetfurkandur <fkn3388@hotmail.com>
2026-03-26 12:05:50 +0300

    chore: Add Docker Compose .dcproj to debugging and gateway Swagger proxy

--- api-gateways/Gateway/Gateway.API/Program.cs
+++ api-gateways/Gateway/Gateway.API/Program.cs
@@ -40,15 +40,17 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/auth/v1/swagger.json", "Auth Service");
+        options.RoutePrefix = "swagger"; // Gateway'de /swagger adresinden erişim
+    });
 }
 
 app.UseAuthentication();
